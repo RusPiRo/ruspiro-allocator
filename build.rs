72526298444d3ee2c7403cf9f2278318a9d34057
Borrmann <43264484+2ndTaleStudio@users.noreply.github.com>
2019-12-14 19:41:47 +0100

    remove unecceary oom "hack", enable aarch64 builds

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
-/*********************************************************************************************************************** 
+/***********************************************************************************************************************
  * Copyright (c) 2019 by the authors
- * 
- * Author: André Borrmann 
+ *
+ * Author: André Borrmann
  * License: Apache License 2.0
  **********************************************************************************************************************/
 //! Build script to pre-compile the heap memory allocation routines written in c for the time beeing
-//! 
+//!
 extern crate cc;
 use std::env;
 
@@ -13,9 +13,7 @@ fn main() {
     match env::var_os("CARGO_CFG_TARGET_ARCH") {
         Some(target_arch) => {
             if target_arch == "arm" {
-                cc::Build::new()
-                    .file("src/memory.c")
-                    .compile("memory");
+                cc::Build::new().file("src/memory.c").compile("memory");
 
                 cc::Build::new()
                     .file("src/asm/aarch32/memset.s")
@@ -27,12 +25,12 @@ fn main() {
                     .file("src/memory.c")
                     .define("AARCH64", None)
                     .compile("memory");
-                
+
                 cc::Build::new()
                     .file("src/asm/aarch64/memset.S")
                     .compile("memset");
             }
         }
-        _ => ()
+        _ => (),
     }
 }
