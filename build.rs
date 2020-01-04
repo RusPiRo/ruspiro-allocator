a44a2b723ae0d8dfb63f6a81b6c90a589d3de40d
Borrmann <43264484+2ndTaleStudio@users.noreply.github.com>
2020-01-04 18:13:59 +0100

    remove custom oom and assembly

--- build.rs
+++ build.rs
@@ -10,27 +10,16 @@ extern crate cc;
 use std::env;
 
 fn main() {
-    match env::var_os("CARGO_CFG_TARGET_ARCH") {
-        Some(target_arch) => {
-            if target_arch == "arm" {
-                cc::Build::new().file("src/memory.c").compile("memory");
-
-                cc::Build::new()
-                    .file("src/asm/aarch32/memset.s")
-                    .compile("memset");
-            }
-
-            if target_arch == "aarch64" {
-                cc::Build::new()
-                    .file("src/memory.c")
-                    .define("AARCH64", None)
-                    .compile("memory");
-
-                cc::Build::new()
-                    .file("src/asm/aarch64/memset.S")
-                    .compile("memset");
-            }
+    if let Some(target_arch) = env::var_os("CARGO_CFG_TARGET_ARCH") {
+        if target_arch == "arm" {
+            cc::Build::new().file("src/memory.c").compile("memory");
         }
-        _ => (),
+
+        if target_arch == "aarch64" {
+            cc::Build::new()
+                .file("src/memory.c")
+                .define("AARCH64", None)
+                .compile("memory");
+        }
     }
 }
